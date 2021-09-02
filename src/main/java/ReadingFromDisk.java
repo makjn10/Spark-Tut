3b842498d65de757afa8ef9a82a0cc86c8484b71
Mayank Jain <makjn10@gmail.com>
2021-09-03 00:40:39 +0530

    Add program for reading input from text file in disk

---
+++ src/main/java/ReadingFromDisk.java
@@ -0,0 +1,32 @@
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.apache.log4j.Level.WARN;
+
+public class ReadingFromDisk {
+    public static void main(String[] args) {
+        System.setProperty("hadoop.home.dir", "C:\\hadoop\\");
+        Logger.getLogger("org.apache").setLevel(WARN);
+
+        SparkConf conf = new SparkConf()
+                .setAppName("StartingSpark")
+                .setMaster("local[*]");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        sc.textFile("C:\\Users\\makjn\\Desktop\\SPARK\\Spark-Tut\\src\\main\\resources\\biglog.txt")
+                .mapToPair(line -> new Tuple2<>(line.split(",")[0], 1))
+                .filter(tuple -> !(tuple._1.equals("level")))
+                .reduceByKey(Integer::sum)
+                .collect()
+                .forEach(System.out::println);
+    }
+}
+
